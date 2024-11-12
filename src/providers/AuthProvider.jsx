9432ece29ab14c0d96284340b1404112423659a5
indroji-mondal <indro.cse.bu@gmail.com>
2024-11-12 20:41:53 +0600

    Added onAuthStateChanged

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,5 +1,5 @@
-import { createUserWithEmailAndPassword } from 'firebase/auth';
-import React, { createContext } from 'react';
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword } from 'firebase/auth';
+import React, { createContext, useState } from 'react';
 import { auth } from '../firebase.init';
 
 export const AuthContext = createContext(null);
@@ -8,12 +8,30 @@ const AuthProvider = ({children}) => {
 
     const name= 'Sagor er Nodi';
 
+    const [user,setUser]= useState(null);
+
     const createUser = (email, password)=>{
         return createUserWithEmailAndPassword(auth, email, password);
     }
+    const signInUser = (email, password) =>{
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+    onAuthStateChanged(auth, currentUser =>{
+
+        if(currentUser){
+            console.log('Currently logged user: ',currentUser);
+            setUser(currentUser);
+        }
+        else{
+            console.log('No user logged in');
+            setUser(null);
+        }
+
+    })
     const authInfo ={
         name,
-        createUser
+        createUser,
+        signInUser,
     }
     return (
        <AuthContext.Provider value={authInfo}>
